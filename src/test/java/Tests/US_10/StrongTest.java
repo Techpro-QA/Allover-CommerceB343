f06154ac2c4ff3356fe0465729e3011a52ad6c6e
Emre <emreowen57@gmail.com>
2025-09-08 00:48:43 +0200

    probe5

--- src/test/java/Tests/US_10/StrongTest.java
+++ src/test/java/Tests/US_10/StrongTest.java
@@ -2,7 +2,6 @@ package Tests.US_10;
 
 import Pages.VendorRegistrationPage;
 import com.github.javafaker.Faker;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,15 +9,12 @@ import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ExtentReportsListener;
 import utilities.ReusableMethods;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 public class StrongTest {
-    @Test
-    public void strongTest() throws IOException {
+
+    @Test(dataProvider = "STRONG",dataProviderClass = DataProvidersUS10.class)
+    public void strongTest(String data) throws IOException {
 
         ExtentReportsListener.extentTestInfo("Allover Commerce sayfasina gidilir");
 
@@ -50,23 +46,18 @@ public class StrongTest {
         ExtentReportsListener.extentTestInfo("Password girisi yapilir");
 
         //Password icin gecerli bir data girilir
-        action.sendKeys(alloverCommercePage.vendorRegistrationPasswordTextBox, "Emre57*").perform();
+        action.sendKeys(alloverCommercePage.vendorRegistrationPasswordTextBox, data).perform();
 
         ExtentReportsListener.extentTestInfo("Password 'Strong' uyarısı doğrulanıyor");
         Assert.assertTrue(alloverCommercePage.strongTextBox.isDisplayed());
 
         //Strong texti ekran görüntüsü reporta ekleniyor
-        Files.createDirectories(Paths.get("target/screenShots"));
-        String date = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss").format(LocalDateTime.now());
-        String path = "target/screenShots/strongTextBox_" + date + ".jpeg";
-        Files.write(Paths.get(path), alloverCommercePage.strongTextBox.getScreenshotAs(OutputType.BYTES));
-        ExtentReportsListener.extentTest.addScreenCaptureFromPath(System.getProperty("user.dir") + "/" + path);
+        US10ScreenUtils.captureScreen("strongTextBox");
 
         //Confirm Password alanına, Password alanına yazılan şifrenin aynısı yazılır.
-        action.sendKeys(alloverCommercePage.vendorRegistrationConfirmPasswordTextBox, "Emre57*").perform();
+        action.sendKeys(alloverCommercePage.vendorRegistrationConfirmPasswordTextBox, data).perform();
 
         // Driver kapatılır
         Driver.quitDriver();
-
     }
 }
