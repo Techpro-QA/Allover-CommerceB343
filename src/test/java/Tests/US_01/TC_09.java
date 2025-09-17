f960f779b7faab168063b0e20774b74eee2e7e6f
Emre <emreowen57@gmail.com>
2025-09-17 13:43:53 +0200

    gokhan

--- src/test/java/Tests/US_01/TC_09.java
+++ src/test/java/Tests/US_01/TC_09.java
@@ -2,6 +2,7 @@ package Tests.US_01;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.ConfigReader;
 import utilities.ExtentReportsListener;
 
@@ -32,7 +33,8 @@ public class TC_09 extends TestBase_US01 {
         alloverCommercePage.registerSingUpButton.click();
         ExtentReportsListener.extentTestInfo("Login olarak sayfaya girilmediği doğrulanır");
         // 8 Kayit isleminin gerceklesmedigi doğrulanir
-        Assert.assertTrue(alloverCommercePage.singOutButton.isEmpty());
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(alloverCommercePage.singOutButton.isEmpty());
         ExtentReportsListener.extentTestInfo("US1 e göre 'Parola en az 8 karakter uzunluğunda olmalıdır. " +
                 "Daha güçlü hale getirmek için büyük ve küçük harfler, sayılar ve ! ? $ % ^ & kullanılmalıdır.'" +
                 "kuralına uygun olmayan tek karakterli parola (1) kabul adildi" +
